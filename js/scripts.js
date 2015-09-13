a1f454e06106e4f10a042eed39b343584a8b552b
Kelli Margaret <kelli.sommerdyke@gmail.com>
2015-09-12 19:13:32 -0700

    Refactored exisiting js code from 70 + lines to less than 20.

--- js/scripts.js
+++ js/scripts.js
@@ -1,79 +1,22 @@
-var romanNumeral = function(number){
-    var stringNumber = number.toString();
-    var newNumber = "";
-    if(number > 3999 || number < 1){
-        newNumber = "Romans do not believe in your number"
-    }else{
-        if(stringNumber[stringNumber.length-4] < 4 && stringNumber[stringNumber.length-4] !== 0){
-            for(var i = 1; i <= stringNumber[stringNumber.length-4]; i++){
-                newNumber += "M";
-                stringNumber -= 1000;
-                stringNumber = stringNumber.toString();
-            }
-        }
-        if(stringNumber[stringNumber.length-3] == 9){
-                newNumber += "CM";
-            }
-        if(stringNumber[stringNumber.length-3] < 9 && stringNumber[stringNumber.length-3] > 4){
-            for(var i = 6; i <= stringNumber[stringNumber.length-3]; i++){
-                newNumber += "D";
-                stringNumber -= 500;
-                stringNumber = stringNumber.toString();
-            }
-        }
-        if(stringNumber[stringNumber.length-3] == 4){
-                newNumber += "CD";
-        }
-        if(stringNumber[stringNumber.length-3] < 4 && stringNumber[stringNumber.length-3] !== 0){
-            for(var i = 1; i <= stringNumber[stringNumber.length-3]; i++){
-                newNumber += "C";
-                stringNumber -= 100;
-                stringNumber = stringNumber.toString();
-            }
-        }
-        if(stringNumber[stringNumber.length-2] == 9){
-                newNumber += "XC";
-        }
-        if(stringNumber[stringNumber.length-2] < 9 && stringNumber[stringNumber.length-2] > 4){
-            for(var i = 6; i <= stringNumber[stringNumber.length-2]; i++){
-                newNumber += "L";
-                stringNumber -= 50;
-                stringNumber = stringNumber.toString();
-            }
-        }
-        if(stringNumber[stringNumber.length-2] == 4){
-                newNumber += "XL";
-            }
-        if(stringNumber[stringNumber.length-2] < 4 && stringNumber[stringNumber.length-2] !== 0){
-            for(var i = 1; i <= stringNumber[stringNumber.length-2]; i++){
-                newNumber += "X";
-                stringNumber -= 10;
-                stringNumber = stringNumber.toString();
-            }
-        }
-        if(stringNumber[stringNumber.length-1] == 9){
-                newNumber += "IX";
-        }
-        if(stringNumber[stringNumber.length-1] < 9 && stringNumber[stringNumber.length-1] > 4){
-            for(var i = 6; i <= stringNumber[stringNumber.length-1]; i++){
-                newNumber += "V";
-                stringNumber -= 5;
-                stringNumber = stringNumber.toString();
-            }
-        }
-        if(stringNumber[stringNumber.length-1] == 4){
-                newNumber += "IV";
-        }
-        if(stringNumber[stringNumber.length-1] < 3 && stringNumber[stringNumber.length-1] !== 0){
-            for(var i = 1; i <= stringNumber[stringNumber.length-1]; i++){
-                newNumber += "I";
-                stringNumber -= 1;
-                stringNumber = stringNumber.toString();
+var romanNumeral = function(num) {
+
+    var romanString = "";
+    var romanNumerals = ["M", "CM", "D", "CD","C","XC","L","XL","X","IX","V","IV","I"];
+    var numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+
+    if ((num > 3999) || (num < 1)) {
+        romanString += "The Romans did not believe in your number!";
+    } else {
+        for (var i = 0; i < numbers.length; i++) {
+            while(num >= numbers[i]) {
+                romanString += romanNumerals[i];
+                num -= numbers[i];
             }
         }
     }
-    return newNumber;
-}
+
+    return romanString;
+};
 
 
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,13 @@
 describe('romanNumeral', function() {
-    it("displays I for the value of 1", function(){
+    it("displays I for the value of 1", function() {
     expect(romanNumeral(1)).to.equal("I");
     });
 
-    it("displays LXVI for the value of 66", function(){
+    it("displays LXVI for the value of 66", function() {
         expect(romanNumeral(999)).to.equal("CMXCIX");
+    });
+
+    it("does not display a number greater than 3999", function() {
+        expect(romanNumeral(4500)).to.equal("The Romans did not believe in your number!");
     });
 });
